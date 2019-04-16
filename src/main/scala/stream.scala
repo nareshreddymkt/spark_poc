c0da47f3c00d75ad765c7bbc245317d90519552b
Naresh Reddy M <nareshreddymkt@gmail.com>
2019-04-16 10:42:25 +0530

    dsf

--- src/main/scala/stream.scala
+++ src/main/scala/stream.scala
@@ -8,13 +8,19 @@ object stream {
     val spark = SparkSession.builder().appName("SparkExample").master("local").getOrCreate()
     import spark.implicits._
     val userSchema = new StructType().add("name", "string").add("age", "integer")
-    val csvDF = spark
+   /* val csvDF = spark
       .readStream
       .option("sep", ";")
       .schema(userSchema) // Specify schema of the csv files
-      .csv("F:\\My_DEV\\Big_Data\\data\\spark_stream") // Equivalent to format("csv").load("/path/to/directory")
-    csvDF.writeStream.format("console").option("truncate","false").start()
+      .csv("F:\\My_DEV\\Big_Data\\data\\spsark_stream") // Equivalent to format("csv").load("/path/to/directory")
+    val query=csvDF.writeStream.format("console").option("truncate","false").start()
+    query.awaitTermination()
+*/
+    val jsonDF = spark
+      .readStream
+      .json("F:\\My_DEV\\Big_Data\\data\\spark_stream\\json\\first.json")
 
+    jsonDF.show()// Equivalent to format("csv").load("/path/to/directory")
 //    csvDf.writeStream.format("console").option("truncate", "false").start()
   }
 }
